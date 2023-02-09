9fe9e326822617680cb02936916b375ff8e3822c
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2023-02-09 03:00:28 -0300

    fix cors

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ const products = require('./src/products.json')
 const stock = require('./src/stock.json')
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/products", (req, res) => {
   return res.json(products);
 });
 
+
 app.get("/stock", (req, res) => {
   return res.json(stock);
 });
 
+app.get("/stock/:id", (req, res) => {
+  const { id } = req.params
+  const formatedIdToNumber = +id 
+  const productStock = stock.find((product) => product.id === formatedIdToNumber)
+
+  return res.status(201).send(productStock)
+});
+
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
-
