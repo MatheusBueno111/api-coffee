9e1b3e5a442c29f1fb6b3347f1664152b3c50f5f
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2023-02-09 01:56:19 -0300

    fix cors

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const cors = require('cors')
 const products = require('./src/products.json')
 const stock = require('./src/stock.json')
 
+const corsOptions = {  
+  origin:'http://localhost/5000',
+  optionsSuccessStatus: 200
+}
 
-app.use(cors({
-  origin:'https://api-coffee-gilt.vercel.app/'
-}))
-
-app.get("/products", (req, res) => {
+app.get("/products", cors(corsOptions), (req, res) => {
   return res.json(products);
 });
 
@@ -18,11 +18,8 @@ app.get("/stock", (req, res) => {
   return res.json(stock);
 });
 
-
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
-
-
 
 module.exports = app;
